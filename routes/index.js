a6971f59a3d68220fee7228d09f30e3308e9a43c
unknown <dongdong.fdd@fudongdong-5440.autonavi.com>
2015-08-11 21:16:30 +0800

    用户登录从数据库取出数据验证，此外，userDAO层好多错误，没有注意要用回调函数处理得到的数据

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var querystring=require("querystring");
+var UserService=require("User");
+var User=require("../node_modules/User/User");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,12 +15,35 @@ router.post("/login",function(req,res,next){
 	var username=req.body.username;
 	var password=req.body.password;
 
-	if(username == password){
-		req.session.uid=username;
-		res.render("loginDone");
-	}else{
-		res.render("login");
-	}
+
+		var userService=new UserService();
+		var user=new User(username,password);
+		console.log("checking...");
+		var callback=function(err,result){
+			console.log("callback doing...");
+			console.log(result);
+			if(err){
+				console.log("err");
+			}else{
+				var one=result[0];
+				console.log("user.getPassword() "+user.getPassword());
+				if(one){
+					if(one.password == user.getPassword()){
+						req.session.uid=username;
+						console.log("login done");
+						res.render("loginDone");
+					}else{
+						console.log("password wrong");
+						res.render("login");
+					}
+				}else{
+					console.log("no user");
+					res.render("login");
+				}
+			}
+		}
+		userService.checkUser(user,callback);
+
 })
 router.get("/logout",function(req,res,next){
 	req.session.uid=null;
