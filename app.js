7cc913096b49f5afcbd22e414325d44d4cf37dc3
unknown <dongdong.fdd@fudongdong-5440.autonavi.com>
2015-08-11 22:23:56 +0800

    添加addArticle页面，并实现功能

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use('/users', users);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  next(err);
+  res.render("404");
 });
 
 // error handlers
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var querystring=require("querystring");
 var UserService=require("User");
 var User=require("../node_modules/User/User");
+var Article=require("../node_modules/Article/Article");
+var ArticleDAO=require("Article");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -44,6 +46,27 @@ router.post("/login",function(req,res,next){
 		}
 		userService.checkUser(user,callback);
 
+})
+router.get("/addArticle",function(req,res,next){
+	res.render("addArticle");
+})
+router.post("/addArticle",function(req,res,next){
+	var title=req.body.title;
+	var content=req.body.content;
+
+	var articleDAO=new ArticleDAO();
+	var article=new Article(title,content);
+	console.log("adding...");
+	var callback=function(err,result){
+		if(err){
+			console.log("err");
+		}else{
+			console.log(result);
+			console.log("&&&&&&&&&&&&");
+			res.render("addArticle");
+		}
+	}
+	articleDAO.addArticle(article,callback);
 })
 router.get("/logout",function(req,res,next){
 	req.session.uid=null;
